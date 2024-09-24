fb6980a91a872e24f343ae875b5141fa923e864b
Liam Andersson <contactliamandersson@gmail.com>
2024-09-25 00:59:58 +0200

    Created main.ts to test the modules

---
+++ main.ts
@@ -0,0 +1,9 @@
+import { reverseString, countCharecters, capitalizeFirstLetter } from "./stringUtils";
+
+const nodeSentence: string = "learning node-js is important for web developers to create high-performance and relaible apps";
+
+console.log(capitalizeFirstLetter(nodeSentence));
+console.log(reverseString(nodeSentence));
+console.log(countCharecters(nodeSentence));
+
+
