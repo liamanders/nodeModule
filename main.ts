9d62f74b27ad043e64839b5789f1b9c79e49699b
Liam Andersson <contactliamandersson@gmail.com>
2024-09-25 01:27:17 +0200

    Wrote the code to log the path of reversed.txt

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { reverseString, countCharecters, capitalizeFirstLetter, modCountCharecters } from "./stringUtils";
 import fs from 'fs';
+import path from 'path';
 
 const nodeSentence: string = "learning node-js is important for web developers to create high-performance and relaible apps";
 
@@ -12,3 +13,5 @@ console.log(modCountCharecters(nodeSentence));
 const reversedSentence: string = reverseString(nodeSentence);
 fs.writeFileSync('reversed.txt', reversedSentence);
 
+const filePath: string = path.join(__dirname, 'reversed.txt');
+console.log(`Reversed sentence has been saved at: ${filePath}`);
